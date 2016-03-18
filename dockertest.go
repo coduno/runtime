8cd19448e3038998ef6f74fb313559cd095be70e
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2016-03-18 11:09:50 +0100

    Always run on port 8081

--- dockertest.go
+++ dockertest.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/coduno/runtime-dummy/controllers"
-	"github.com/coduno/runtime-dummy/env"
 	"google.golang.org/appengine"
 )
 
 func main() {
 	http.Handle("/", controllers.Router())
-	if env.IsDevAppServer() {
-		fmt.Println("DEV SERVER")
-		os.Setenv("PORT", "8081")
-	}
+	os.Setenv("PORT", "8081")
 	appengine.Main()
 }

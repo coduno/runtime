d4f2b1544c4429ed6a1d9bd7f6cd5e0eea9add80
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2016-04-06 04:21:06 +0200

    Wrap http.DefaultServeMux with a thin logger

--- dockertest.go
+++ dockertest.go
@@ -11,10 +11,17 @@ import (
 
 const sock = ":8081"
 
+func logged(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	go runner.Scrape(10 * time.Minute)
 
 	http.Handle("/", controllers.Router())
 	log.Println("Listening at", sock)
-	log.Fatal(http.ListenAndServe(sock, nil))
+	log.Fatal(http.ListenAndServe(sock, logged(http.DefaultServeMux)))
 }

f4e6471ab27bacae1e2abe4f5f6663ad08dee165
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2016-04-07 11:19:30 +0200

    Simplify runners

--- model/test_stats.go
+++ model/test_stats.go
@@ -1,7 +1,8 @@
 package model
 
 type TestStats struct {
-	Stdout     string `json:"stdout,omitempty"`
-	Stderr     string `json:"stderr,omitempty"`
-	Successful bool   `json:"successful,omitempty"`
+	Stdout     string      `json:"stdout,omitempty"`
+	Stderr     string      `json:"stderr,omitempty"`
+	Successful bool        `json:"successful,omitempty"`
+	Stats      interface{} `json:"stats,omitempty"`
 }
